6cf9d9adab4be707fd6bb1f38f70b866464ec2ba
artem <arte@kb.ri>
2022-07-19 16:47:56 +0300

    Initial Commit

---
+++ MusicPlayers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayers
+//
+//  Created by Артем Орлов on 19.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
